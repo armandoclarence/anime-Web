c5e6d6743dfe0763fdef7c7622771d22fa42293a
armandoclarence <aclarence200@gmail.com>
2023-10-04 22:55:22 +0700

    making card box

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const search = document.querySelector('#search')
 const baseUrl = "https://anime-api.cyclic.cloud/"
 
+const container = document.querySelector('.container')
 search.addEventListener('change', function(){
     
     let q = search.value
     fetch(`${baseUrl}search?q=${q}`)
     .then(res => res.json())
-    .then(({datas}) => {
-        datas.foreach(data => {
-            const container = document.querySelector(".container")
-
-            container.html = `<ul></ul>`
-            console.log(data)
+    .then(({data}) => 
+        data.forEach(animeData => {
+            const {title, thumbnail, slug, genres, rating } = animeData
+            container.innerHTML += `<article>
+                <h2>${title}</h2>
+                <img src="${thumbnail}" alt="anime..." />
+                <p>${slug}</p>
+                <ul>
+                    ${genres.map(genre => {
+                        return `<li>${genre}</li>`  
+                    })}
+                </ul>
+            </article>`
+            console.log(container)
+            console.log(animeData)
         })
-    })
+        // container.innerHTML = ''
+    )
 })
 
 
