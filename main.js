0fd56917a5808f90f411a7214b52cf556ca651a1
armandoclarence <aclarence200@gmail.com>
2023-10-05 23:14:54 +0700

    add

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
 const search = document.querySelector('#search')
+const cardDetail = document.querySelector('.cardDetail')
 const baseUrl = "https://anime-api.cyclic.cloud/"
 
-const container = document.querySelector('.container')
+const container = document.querySelector('.container-card')
 search.addEventListener('change', function(e){
     e.preventDefault()
     let q = search.value
@@ -13,26 +14,45 @@ function getAnimeByQuery(q){
     fetch(`${baseUrl}search?q=${q}`)
     .then(res => res.json())
     .then(({data}) => 
-        data.forEach(animeData => {
-            const {title, genres, rating, thumbnail, slug} = animeData
-            container.innerHTML += `<article>
+        data.map(animeData => {
+            const {title, thumbnail, genres, rating, slug} = animeData
+            container.innerHTML += `
+                <article class="card">
                 <h2>${title}</h2>
                 <img src="${thumbnail}" alt="anime..." />
                 <ul>
-                    ${genres.map(genre => {
-                        return `<li>${genre}</li>`  
-                    })}
+                ${genres.map(genre => {
+                    return `<li>${genre}</li>`  
+                })}
                 </ul>
                 <p>rating: ${rating}</p>
-            </article>`
+                </article>`
             getAnimeDetail(slug)
         }))
-}
+    }
 function getAnimeDetail(slug){
+    const cards = document.querySelectorAll('.card')
+    cards.forEach(card => card.addEventListener('click',function(){
+        console.log(this)
+        cardDetail.innerHTML = `<ul>
+            <li className="1"></li>
+            <li className="2"></li>
+            <li className="3"></li>
+            <li className="4"></li>
+            <li className="5"></li>
+            <li className="6"></li>
+            <li className="7"></li>
+            <li className="8"></li>
+            <li className="9"></li>
+            <li className="10"></li>
+        </ul>`
+    }))
     fetch(`${baseUrl}anime/${slug}`)
     .then(res => res.json())
     .then((data) => {
-        console.log(data)
-    })
+        const {episodeLists} = data
+        console.log(data);
+        console.log(episodeLists)
+   })
 }
 
