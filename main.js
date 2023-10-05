8412b1222c294f73a372e507989f2169542750f3
armandoclarence <aclarence200@gmail.com>
2023-10-05 19:07:21 +0700

    adding style card box

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
+
 const search = document.querySelector('#search')
 const baseUrl = "https://anime-api.cyclic.cloud/"
 
 const container = document.querySelector('.container')
-search.addEventListener('change', function(){
-    
+search.addEventListener('change', function(e){
+    e.preventDefault
     let q = search.value
     fetch(`${baseUrl}search?q=${q}`)
     .then(res => res.json())
     .then(({data}) => 
         data.forEach(animeData => {
-            const {title, thumbnail, slug, genres, rating } = animeData
+            const {title, thumbnail, genres, rating } = animeData
+    
             container.innerHTML += `<article>
                 <h2>${title}</h2>
                 <img src="${thumbnail}" alt="anime..." />
-                <p>${slug}</p>
                 <ul>
                     ${genres.map(genre => {
                         return `<li>${genre}</li>`  
                     })}
                 </ul>
+                <p>rating: ${rating}</p>
             </article>`
             console.log(container)
             console.log(animeData)
         })
-        // container.innerHTML = ''
     )
 })
 
