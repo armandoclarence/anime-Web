b5e507214ea153f914b1a9d157d1a3880cb64908
armandoclarence <aclarence200@gmail.com>
2023-10-06 22:58:02 +0700

    add anime detail

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const search = document.querySelector('#search')
-const cardDetail = document.querySelector('.cardDetail')
+const cardDetail = document.querySelector('.card-detail')
+const modal = document.querySelector('[data-modal]')
 const baseUrl = "https://anime-api.cyclic.cloud/"
 
 const container = document.querySelector('.container-card')
@@ -10,16 +11,19 @@ search.addEventListener('change', function(e){
     getAnimeByQuery(q)
 })
 
-function getAnimeByQuery(q){
-    fetch(`${baseUrl}search?q=${q}`)
-    .then(res => res.json())
-    .then(({data}) => 
-        data.map(animeData => {
-            const {title, thumbnail, genres, rating, slug} = animeData
-            container.innerHTML += `
-                <article class="card">
+async function getAnimeByQuery(q){
+    const anime = await fetch(`${baseUrl}search?q=${q}`)
+    const animeData = await anime.json()
+    const {data} = animeData;
+    makeCard(data)
+}
+function makeCard(datas){
+    datas.map(data => {
+        const {title, thumbnail, slug, genres, rating} = data
+        container.innerHTML += `
+            <article class="card" value=${slug}>
                 <h2>${title}</h2>
-                <img src="${thumbnail}" alt="anime..." />
+                <img src="${thumbnail}" alt="${slug}" />
                 <ul>
                 ${genres.map(genre => {
                     return `<li>${genre}</li>`  
@@ -27,32 +31,44 @@ function getAnimeByQuery(q){
                 </ul>
                 <p>rating: ${rating}</p>
                 </article>`
-            getAnimeDetail(slug)
-        }))
-    }
-function getAnimeDetail(slug){
+            })
     const cards = document.querySelectorAll('.card')
     cards.forEach(card => card.addEventListener('click',function(){
-        console.log(this)
-        cardDetail.innerHTML = `<ul>
-            <li className="1"></li>
-            <li className="2"></li>
-            <li className="3"></li>
-            <li className="4"></li>
-            <li className="5"></li>
-            <li className="6"></li>
-            <li className="7"></li>
-            <li className="8"></li>
-            <li className="9"></li>
-            <li className="10"></li>
-        </ul>`
+        let slug = this.getAttribute('value')
+        getAnimeDetail(slug)
+        modal.showModal()
     }))
+}
+
+function getAnimeDetail(slug){
     fetch(`${baseUrl}anime/${slug}`)
     .then(res => res.json())
     .then((data) => {
-        const {episodeLists} = data
-        console.log(data);
-        console.log(episodeLists)
+        const {title, thumbnail, genres, score,episodeLists} = data
+        cardDetail.innerHTML =` <article class="card">
+            <h2>${title}</h2>
+            <img src="${thumbnail}" alt="" />
+            <ul>
+            ${genres.map(genre => {
+                return `<li>${genre}</li>`  
+            })}
+            </ul>
+            <p>rating: ${score}</p>
+            <ul>
+                ${episodeLists.map(episodeList =>{
+                    const { slug } = episodeList
+                    let i = slug.replace(/\D/g, '')
+                    return `<li>
+                        <input data-value="${slug}" type="button" value="${i}" />
+                    </li>`
+                }).join('')}
+            </ul>
+            <input id="backButton" type="button" value="Kembali" />
+        </article>`
+        const backButton = document.querySelector('#backButton')
+        backButton.addEventListener('click',function(){
+            modal.close()
+        })
    })
 }
 
