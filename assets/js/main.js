b39b1a5dfbbd9c3b63be175de126f4c0f8f6b25e
armandoclarence <aclarence200@gmail.com>
2023-11-14 22:52:56 +0700

    l

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
 const prevButton = document.querySelector('.prev')
 const nextButton = document.querySelector('.next')
 
-const bar = document.querySelector('.bars')
+const bar = document.querySelector('.bar')
 const navLink = document.querySelector('#nav ul')
+
+navLink.classList.toggle('hidden')
+
+bar.addEventListener('click', function(e){
+    navLink.classList.toggle('hidden')
+})
 
 export { prevButton, nextButton}
