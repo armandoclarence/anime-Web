9ab4dc8686fdccee6298bd5a33a0bcb76e918fd9
armandoclarence <aclarence200@gmail.com>
2023-11-21 23:09:49 +0700

    .

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -16,7 +16,13 @@ listButtons.map(listButton => {
     listButton.addEventListener('click',function(){
         let id = this.getAttribute('id')
         const listFilter = document.querySelector(`ul#${id}`)
-        console.log(this.getAttribute('id'))
+        const [...notTargetFilter] = document.querySelectorAll(`.lists ul:not(#${id})`)
         listFilter.classList.toggle('hidden')
+        listFilter.style.top = `${this.offsetTop * 2 || this.offsetHeight + 5}px`
+        listFilter.style.left =`${this.offsetLeft}px`
+        console.log(listFilter.style)
+        notTargetFilter.map(notTarget => {
+            notTarget.classList.add('hidden')
+        })
     })
 })
